f4d4d28e7961463f6c5345209b2266ef29ed16f7
bobbyZhong10 <126168729+bobbyZhong10@users.noreply.github.com>
2024-04-29 16:35:03 +0800

    Update Study1 @0429

--- Study1模型比较.py
+++ Study1模型比较.py
@@ -14,7 +14,7 @@ from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
 sns.set(style="whitegrid")
 
 # 读取数据
-data_path = 'panel data.csv'
+data_path = 'PanelData@0429.csv'
 data = pd.read_csv(data_path)
 
 # 准备数据列
@@ -59,7 +59,7 @@ def objective(params):
     return {'loss': -score, 'status': STATUS_OK}
 
 trials = Trials()
-best_params = fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=500, trials=trials)
+best_params = fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=100, trials=trials)
 best_params['n_estimators'] += 20
 best_params['max_depth'] += 3
 best_params['min_samples_split'] += 2
